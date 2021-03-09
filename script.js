62d4abb7af272ce34351f2edc8d4f7003942b170
Huggo Parcelly <h.parcelly@gmail.com>
2021-03-09 19:27:10 -0300

    Adiciona altura e largura dos pixels

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
 window.onload = function () {
-  let pixelBox = document.querySelector('#pixel-board'); //captura o quadro completo
-
+  const pixelBox = document.querySelector('#pixel-board'); // captura o quadro completo
   function createPixel() {
-    let pix = document.createElement('div');
+    const pix = document.createElement('div');
     pix.className = 'pixel';
     return pix;
   }
-
   function createLine() {
-    let lines = document.createElement('div');
+    const lines = document.createElement('div');
     lines.className = 'line';
     return lines;
   }
-
-  for (l = 0; l < 5; l += 1) {
+  for (let l = 0; l < 5; l += 1) {
     pixelBox.appendChild(createLine());
-    for (pixel = 0; pixel < 5; pixel += 1) {
-      let getLine = document.querySelectorAll('.line');
+    for (let pixel = 0; pixel < 5; pixel += 1) {
+      const getLine = document.querySelectorAll('.line');
       getLine[l].appendChild(createPixel());
     }
   }
-
-  
-}
-  
+};

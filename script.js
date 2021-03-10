dfa8dfce584751bf88731957bb4cfe01d860a186
Huggo Parcelly <h.parcelly@gmail.com>
2021-03-10 13:56:25 -0300

    Adiicona a troca da class color para color selected quando o elemento eh clicado

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-window.onload = function () {
+function createPixel() {
+  const pix = document.createElement('div');
+  pix.className = 'pixel';
+  return pix;
+}
+
+function createLine() {
+  const lines = document.createElement('div');
+  lines.className = 'line';
+  return lines;
+}
+
+function createAllPixel() {
   const pixelBox = document.querySelector('#pixel-board'); // captura o quadro completo
-  function createPixel() {
-    const pix = document.createElement('div');
-    pix.className = 'pixel';
-    return pix;
-  }
-  function createLine() {
-    const lines = document.createElement('div');
-    lines.className = 'line';
-    return lines;
-  }
   for (let l = 0; l < 5; l += 1) {
     pixelBox.appendChild(createLine());
     for (let pixel = 0; pixel < 5; pixel += 1) {
@@ -17,4 +19,24 @@ window.onload = function () {
       getLine[l].appendChild(createPixel());
     }
   }
+}
+
+function selectedBlack() {
+  const getBlack = document.getElementById('black');
+  getBlack.classList.add('selected'); // adiciona o selected na classe da cor preta
+}
+
+function selectedColor() {
+  const getClassColor = document.querySelectorAll('.color');
+  for (let i = 0; i < getClassColor.length; i += 1) { // interação para clicar em todos os elementos da palleta
+    getClassColor[i].addEventListener('click', function () {
+      this.classList.add('selected');
+    });
+  }
+}
+
+window.onload = function () {
+  createAllPixel();
+  selectedBlack();
+  selectedColor();
 };
